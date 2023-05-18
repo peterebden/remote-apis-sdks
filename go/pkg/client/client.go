357e925feee1108e1d51cb21f761b2077988e9c0
Peter Ebden <peter.ebden@gmail.com>
2023-05-18 13:24:52 +0100

    Support 'packs' for compressed uploads/downloads

--- go/pkg/client/client.go
+++ go/pkg/client/client.go
@@ -179,6 +179,8 @@ type Client struct {
 	UnifiedDownloadBufferSize UnifiedDownloadBufferSize
 	// UnifiedDownloadTickDuration specifies how often the unified download daemon flushes the pending requests.
 	UnifiedDownloadTickDuration UnifiedDownloadTickDuration
+	// PackName defines the node property name that the client will create and upload 'packs' as.
+	PackName string
 	// TreeSymlinkOpts controls how symlinks are handled when constructing a tree.
 	TreeSymlinkOpts *TreeSymlinkOpts
 
@@ -464,6 +466,22 @@ type PerRPCCreds struct {
 // Apply saves the per-RPC creds in the Client.
 func (p *PerRPCCreds) Apply(c *Client) {
 	c.creds = p.Creds
+}
+
+
+// PackName defines the node property name that the client will create and upload 'packs' as
+// for output directories. These are tarballs containing a complete form of that tree which
+// can be downloaded to short-circuit fetching a potentially large tree of files.
+type PackName struct {
+	Name string
+}
+
+func (p *PackName) Apply(c *Client) {
+	c.PackName = p.Name
+}
+
+func UsePackName(name string) *PackName {
+	return &PackName{Name: name}
 }
 
 func getImpersonatedRPCCreds(ctx context.Context, actAs string, cred credentials.PerRPCCredentials) credentials.PerRPCCredentials {
